811f2f9cfbb9059d20e3e5be2615677cfe76ad0c
muslimumarov <muslimbekaxmatovich@gmail.com>
2025-04-08 20:10:46 +0500

    js darsalar

---
+++ index.js
@@ -0,0 +1,11 @@
+// const firstName = 'Muslim'
+// const lastName = "Umarov"
+// const age = 27
+// console.log(`My name is ${firstName}, My Last Name ${lastName}, my age ${age}`);
+// const isMarrid = true
+// const Nomarrid = false
+// const sr = false
+// const how = isMarrid || Nomarrid  && sr
+// console.log(how);
+// const how = prompt("how old are you", "")
+// console.log(`I am ${how} yaars old`);
