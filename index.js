a63393b0d99650a054c4d3a1b81dc8be307e249a
muslimumarov <norbekumarov613gmail.com>
2025-04-12 18:18:43 +0500

    kdfdjkfA

--- index.js
+++ index.js
@@ -8,4 +8,112 @@
 // const how = isMarrid || Nomarrid  && sr
 // console.log(how);
 // const how = prompt("how old are you", "")
-// console.log(`I am ${how} yaars old`);
+// // console.log(`I am ${how} yaars old`);
+// const serial = +prompt(`nechta serila ko'rdingiz?`, " ")
+// const obj ={
+//   count : serial,
+//   series : [],
+// }
+// const a = prompt('qaysi serial kording', '')
+// const b = +prompt('nechhi baho berasiz', " ")
+//  obj.series[a] = b
+//  console.log(obj);
+
+// const rang = prompt("Rang kirit", 'rang kirit')
+// // if(Svetafor == "qizil") {
+// //   console.log('Stop')
+// // } else if (Svetafor == "sariq") {
+// //   console.log("tayorlan")
+// // } else  if(Svetafor == "yashil") {
+// //   console.log('bos')
+// // } else {
+// //   console.log('yo, yashil, yo, sariq , yo qizil, rang kirit')
+// // }
+// // const a = +prompt('yosh kirit', '')
+// // if(a > 18) {
+// //   console.log('ruxsat')
+// // } else if (a >= 18) {
+// //   console.log('ha mayli')
+// // } else if(a < 18) {
+// //   console.log('ukam yoshsan')
+// // }
+// // else {
+// //   console.log('son kirting')
+// // }
+// // type
+// // const a  = 33
+// // console.log(a === "33");
+// switch(rang) {
+//   case "red":
+//    console.log('stop');
+//    break
+//    case "yellow":
+//     console.log('tayorlan')
+//     break
+//     case "blue":
+//       console.log('gov')
+//       default:
+//
+// }
+
+// const a = prompt('fasl kirit', "")
+// switch(a) {
+//   case "yanvar":
+//     console.log('hozir qish');
+//     break
+//     case "mart":
+//       console.log('hozr bahor')
+//       break
+//       case "August":
+//         console.log('hozr yoz')
+//         break
+//         case "sentabr":
+//           console.log("hozr kuz")
+//           break
+//           default:
+//             console.log('xatolik')
+// }
+// let num = 1
+// while(num <= 5 ) {
+//   console.log(num);
+//   num++
+
+// }
+// do {
+// console.log(num);
+// num++
+// // } while(num <= 5)
+// for (num; num <= 5; num++) {
+//   if(num == 2) {
+//     // break
+//     continue
+//   }
+//   console.log(num);
+
+// }
+
+const kino = prompt('nechta serila kordingiz')
+
+const obj = {
+  count: kino,
+  series: {},
+  actors: [],
+}
+for (let i = 0; i < 3; i++) {
+  const a = prompt(`qaysi serialni kordingiz ${i}`, "")
+  const b = prompt(`nechchi baho qoyasz ${i}`, "")
+  obj.series[a] = b
+  if(a !== null && b !== null && a !=="" && b !=="") {
+    console.log('DONE')
+  } else {
+    i--
+  }
+}
+
+if(obj.count < 1 ) {
+  console.log('siz kam kino korarkansz')
+} else if(obj.count < 5 ) {
+  console.log('siz klsasic')
+} else if(obj.count  > 10 )
+ {console.log('siz zvezdasz')}
+console.log(obj);
